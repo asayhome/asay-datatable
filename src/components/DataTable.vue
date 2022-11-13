1c4a2b025cc37e107ae1e493382c0f90e8953ed5
Abdelrahman Saeed Elhassan <abdosaeedelhassan@gmail.com>
2022-11-13 15:08:31 +0200

    updates

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,304 +1,284 @@
 <template>
-  <div class="bg-white shadow rounded-[10px] p-[10px]" style="width: 100%">
-    <div
-      style="
-        display: flex;
-        flex-direction: column;
-        gap: 5px;
-        width: 100%;
-        position: relative;
-      "
-    >
-      <div
-        style="
-          display: flex;
-          width: 100%;
-          justify-content: space-between;
-          gap: 5px;
-          flex-flow: wrap;
-        "
-      >
-        <Search v-model="search" :locale="locale" />
-        <slot name="datatable-actions" />
-        <Export :locale="locale" @exportTo="exportTo" />
-      </div>
-      <div style="overflow-x: auto; width: 100%">
-        <table :id="id">
-          <thead>
-            <th
-              v-for="column in columns"
-              :key="column.name"
-              style="padding: 10px; font-weight: bold"
+  <div class="card card-p-0 card-flush">
+    <div class="card-header align-items-center py-5 gap-2 gap-md-5">
+      <div class="card-title">
+        <div class="d-flex align-items-center position-relative my-1">
+          <span class="svg-icon svg-icon-1 position-absolute ms-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
             >
-              {{ __(column.label) }}
-            </th>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(row, rowIndex) in tableRows"
-              :key="rowIndex"
-              :data-index="rowIndex"
-            >
-              <td
-                v-for="column in columns"
-                :key="column.name"
-                style="padding: 10px"
-              >
-                <slot :name="column.name" :item="row" :index="column.name">
-                  <div style="" v-html="row[column.name]"></div>
-                </slot>
-              </td>
-            </tr>
-            <tr v-if="!tableRows.length">
-              <td :colspan="columns.length" style="padding: 10px">
-                <span>{{ trans("No records added yet", locale) }}</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div style="margin-top: 10px; width: 100%" v-if="recordsTotal > 0">
-          <Paginate
-            :page="page"
-            :start="start"
-            :paginate="paginate"
-            :total="recordsFiltered"
-            @setPaginateData="setPaginateData"
-            :locale="locale"
+              <rect
+                opacity="0.5"
+                x="17.0365"
+                y="15.1223"
+                width="8.15546"
+                height="2"
+                rx="1"
+                transform="rotate(45 17.0365 15.1223)"
+                fill="black"
+              ></rect>
+              <path
+                d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+                fill="black"
+              ></path>
+            </svg>
+          </span>
+          <!--end::Svg Icon-->
+          <input
+            type="text"
+            :id="id + 'searchBox'"
+            class="form-control form-control-solid w-250px ps-14"
+            :placeholder="__('Search her')"
           />
         </div>
+        <slot name="actions" />
       </div>
-      <Spinner :show="isLoading" />
+      <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
+        {{ __("Export") }}
+        <div class="dt-buttons btn-group flex-wrap">
+          <button
+            class="btn btn-secondary buttons-copy buttons-html5"
+            @click="exportAs('0')"
+            type="button"
+          >
+            <span>{{ __("Copy") }}</span>
+          </button>
+          <button
+            class="btn btn-secondary buttons-excel buttons-html5"
+            @click="exportAs('1')"
+            type="button"
+          >
+            <span>{{ __("Excel") }}</span>
+          </button>
+          <button
+            class="btn btn-secondary buttons-csv buttons-html5"
+            @click="exportAs('2')"
+            type="button"
+          >
+            <span>{{ __("CSV") }}</span>
+          </button>
+          <button
+            class="btn btn-secondary buttons-pdf buttons-html5"
+            @click="exportAs('3')"
+            type="button"
+          >
+            <span>{{ __("PDF") }}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="card-body">
+      <table
+        class="table table-hover table-bordered rounded fs-6 g-5 mt-5"
+        :id="id"
+      >
+        <thead>
+          <tr class="text-start text-gray-400 fw-bolder text-uppercase">
+            <th v-for="column in columns" :key="column.id">
+              {{ __(column.label) }}
+            </th>
+          </tr>
+        </thead>
+        <tbody class="fw-bold text-gray-600"></tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import { ref } from "vue";
-import Paginate from "./Paginate.vue";
-import _ from "lodash";
-import qs from "qs";
-import Export from "./Export.vue";
-import Search from "./Search.vue";
-import Spinner from "./Spinner.vue";
-
+import arLocale from "../Translations/Ar.json";
+import enLocale from "../Translations/En.json";
 export default {
   props: {
-    id: {
-      default: "AsayDataTable",
-    },
     columns: Array,
     ajaxUrl: String,
     params: Object,
-    data: Array,
-    paginate: {
-      type: Number,
-      default: 10,
+    action: {
+      default: "get",
     },
     locale: {
       default: "ar",
     },
-    showWaitLoader: {
-      type: Boolean,
-      default: false,
+    id: {
+      default: "kt_datatable_example_1",
     },
   },
-  setup() {
-    let tableRows = ref([]);
-    let links = ref([]);
-    let recordsFiltered = ref(0);
-    let recordsTotal = ref(0);
-    let startWith = ref(0);
-    let page = ref(1);
-    let search = ref(null);
-    let isLoading = ref(false);
+  setup(props) {
+    let dataTable = ref(null);
+    let exportButtons = ref([]);
+    let passedParams = ref(props.params);
     return {
-      tableRows,
-      links,
-      recordsFiltered,
-      recordsTotal,
-      page,
-      startWith,
-      search,
-      isLoading,
+      dataTable,
+      exportButtons,
+      passedParams,
     };
   },
-  computed: {
-    passedParams: function () {
-      let cols = this.columns.map((col) => {
-        return {
-          name: col.name,
-          data: col.name,
-          searchable: col?.searchable ? col.searchable : true,
-          orderable: col?.searchable ? col.orderable : true,
-          search: {
-            value: null,
-            regex: false,
-          },
-        };
-      });
-
-      let params = {
-        draw: parseInt(this.page),
-        start: this.startWith,
-        length: parseInt(this.paginate),
-        columns: cols,
-        search: {
-          value: this.search,
-          regex: false,
-        },
-        _: "" + Date.now(), // current timestamp
-      };
-
-      if (this.params) {
-        Object.entries(this.params).forEach(([key, value]) => {
-          params[key] = value;
-        });
-      }
-
-      return params;
-    },
-  },
-  watch: {
-    params: function (newVal) {
-      // console.log(newVal);
-      // this.reloadData();
-    },
-    page: function (newVal) {
-      this.reloadData();
-    },
-    search: _.debounce(function () {
-      this.reloadData();
-    }, 1000),
-  },
   methods: {
-    exportTo(type) {
-      if (type == "pdf") {
-        console.log(type);
-      }
+    exportAs(index) {
+      this.dataTable.button(index).trigger();
     },
-    setPaginateData(data) {
-      this.page = data.page;
-      this.startWith = data.startWith;
+    setColsVisibilty(columns, visible) {
+      this.dataTable.columns(columns).visible(visible);
     },
-    addActionsListener() {
-      let vm = this;
-      document.querySelectorAll("#" + this.id + " tbody tr").forEach((row) => {
-        row.querySelectorAll("a").forEach((action) => {
-          action.addEventListener("click", function (actionEvent) {
-            actionEvent.preventDefault();
-            /**
-             * Extracting data
-             */
-            let index = row.getAttribute("data-index");
-            let action = this.getAttribute("data-action");
-            let route = this.getAttribute("data-route");
-            let rowData = vm.tableRows?.[index];
-            let clicked = {
-              action: action,
-              route: route,
-              data: rowData,
-            };
-            vm.$emit("setAction", clicked);
+    getExportableColumns() {
+      let columns = [];
+      for (let i = 0; i < this.columns.length; i++) {
+        if (this.columns[i].hasOwnProperty("exportable")) {
+          if (this.columns[i].exportable) {
+            columns.push({
+              name: this.columns[i].name,
+              index: i,
+            });
+          }
+        } else {
+          columns.push({
+            name: this.columns[i].name,
+            index: i,
           });
-        });
+        }
+      }
+      return columns;
+    },
+    initDataTable() {
+      let vm = this;
+      this.dataTable = $("#" + this.id).DataTable({
+        searching: true,
+        searchDelay: 500,
+        processing: true,
+        serverSide: true,
+        //order: [[0, "asc"]],
+        stateSave: true,
+        // select: {
+        //   style: "multi",
+        //   selector: 'td:first-child input[type="checkbox"]',
+        //   className: "row-selected",
+        // },
+        ajax: {
+          type: this.action,
+          url: this.ajaxUrl,
+          dataType: "json",
+          cache: false,
+          data: function (d) {
+            $.extend(d, vm.passedParams);
+          },
+        },
+        columns: this.columns.map((col) => {
+          col.data = col.name;
+          return col;
+        }),
+        pagingType: "full_numbers",
+        language: this.locale == "ar" ? arLocale : enLocale,
+        scrollY: true,
+        buttons: [
+          "copy",
+          "csv",
+          "excel",
+          {
+            extend: "print",
+            exportOptions: {
+              columns: this.getExportableColumns().map(
+                (column) => column.index
+              ),
+            },
+            customize: function (win) {
+              win.document.body.setAttribute("dir", "rtl");
+            },
+          },
+        ],
+      });
+      /**
+       * Handle search box
+       */
+      //const filterSearch = document.querySelector('[data-kt-filter="search"]');
+      //filterSearch.addEventListener("keyup", function (e) {
+      //  vm.dataTable.search(e.target.value).draw();
+      //});
+      // $(this.id + "searchBox").keyup(function () {
+      //   vm.dataTable.search(e.target.value).draw();
+      // });
+      $("#" + this.id + "searchBox").on("keyup", function (e) {
+        vm.dataTable.search(e.target.value).draw();
+      });
+      // // Hook dropdown menu click event to datatable export buttons
+      // const exportButtons = document.querySelectorAll(
+      //   "#kt_datatable_example_1_export_menu [data-kt-export]"
+      // );
+      // exportButtons.forEach((exportButton) => {
+      //   exportButton.addEventListener("click", (e) => {
+      //     e.preventDefault();
+      //     // Get clicked export value
+      //     const exportValue = e.target.getAttribute("data-kt-export");
+      //     const target = document.querySelector(
+      //       ".dt-buttons .buttons-" + exportValue
+      //     );
+      //     // Trigger click event on hidden datatable export buttons
+      //     target.click();
+      //   });
+      // });
+      /**
+       * Handle actions
+       */
+      $("#" + this.id + " tbody").on("click", "a", function (e) {
+        let action = $(this).data("action");
+        let route = $(this).data("route");
+        let clicked = {
+          action: action,
+          route: route,
+          data: vm.dataTable.row($(this).parents("tr")).data(),
+        };
+        vm.$emit("setAction", clicked);
       });
     },
     reloadData() {
-      if (this.ajaxUrl) {
-        if (this.showWaitLoader) {
-          this.isLoading = true;
-        }
-        axios
-          .get(this.ajaxUrl, {
-            params: this.passedParams,
-            paramsSerializer: (params) => {
-              return qs.stringify(params, {
-                charset: "utf-8",
-                interpretNumericEntities: true,
-              });
-            },
-          })
-          .then((res) => {
-            if (res.status == 200) {
-              setTimeout(() => {
-                this.isLoading = false;
-              }, 500);
-              this.recordsFiltered = parseInt(res.data.recordsFiltered);
-              this.recordsTotal = parseInt(res.data.recordsTotal);
-              this.start = parseInt(res.data.input?.start);
-              this.page = parseInt(res.data.draw);
-              this.tableRows = res.data.data;
-              this.$emit("renderingComplete");
-              setTimeout(() => {
-                this.addActionsListener();
-              }, 1000);
-              console.log(res);
-            }
-          })
-          .catch((error) => {
-            setTimeout(() => {
-              this.isLoading = false;
-            }, 500);
-          });
-      } else {
-        this.tableRows = this.data;
-      }
+      this.dataTable?.ajax.reload();
     },
-    setColumnVisible(col_no, visible) {
-      let table = document.getElementById(this.id);
-      var rows = table.rows;
-      for (var row = 0; row < rows.length; row++) {
-        var cols = rows[row].cells;
-        if (col_no >= 0 && col_no < cols.length) {
-          cols[col_no].style.display = visible ? "" : "none";
-        }
-      }
-      document.getElementsByTagName("th")[col_no].style.display = visible
-        ? ""
-        : "none";
+    reRenderTable() {
+      $("#" + this.id)
+        .DataTable()
+        .destroy();
+      this.initDataTable();
     },
+  },
+  created() {
+    //this.$loadScript("/demo1/plugins/custom/datatables/datatables.bundle.js")
+    //.then(() => {
+    // Script is loaded, do something
+    // this.initDataTable();
+    //})
+    //.catch(() => {
+    //  // Failed to fetch script
+    //});
   },
   mounted() {
-    this.reloadData();
+    //this.$loadScript("/demo1/plugins/custom/datatables/datatables.bundle.js")
+    //.then(() => {
+    // Script is loaded, do something
+    this.initDataTable();
+    //})
+    //.catch(() => {
+    //  // Failed to fetch script
+    //});
+    $.ajaxSetup({
+      headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+      },
+    });
   },
-  components: { Paginate, Export, Search, Spinner },
+  watch: {
+    params: function (newVal, oldVal) {
+      this.passedParams = newVal;
+      this.reloadData();
+    },
+  },
 };
 </script>
-
-<style scoped>
-.bg-white {
-  --tw-bg-opacity: 1;
-  background-color: rgb(255 255 255 / var(--tw-bg-opacity));
-}
-.shadow {
-  --tw-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
-  --tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color),
-    0 1px 2px -1px var(--tw-shadow-color);
-  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
-    var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
-}
-.rounded-\[10px\] {
-  border-radius: 10px;
-}
-.p-\[10px\] {
-  padding: 10px;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-  width: 100%;
-  border: 1px solid gray;
-}
-th,
-td {
-  padding: 8px;
-  border: 1px solid gray;
-}
-tr:nth-child(even) {
-  background-color: #f2f2f2;
-}
-tr:hover {
-  background-color: #fffdf3;
+<style>
+.dataTables_filter {
+  display: none;
 }
 </style>
